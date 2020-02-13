05081d88523d4337619e707062ee323ea71df5da
iuliancarnaru <iulian.carnaru@gmail.com>
2020-02-13 09:52:13 +0000

    adding boilerplate for further projects

---
+++ boilerplate/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+import "normalize.css";
+import "./styles/index.scss";
+
+function Boilerplate() {
+  return <div>Boilerplate</div>;
+}
+
+render(<Boilerplate />, document.getElementById("root"));
---
+++ boilerplate/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const WebpackBar = require("webpackbar");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [new WebpackBar()],
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    stats: "errors-warnings",
+    port: 9000
+  }
+};
