0641d30db366c86c6edd95b40ec3c49b321931c5
iuliancarnaru <iulian.carnaru@gmail.com>
2020-02-13 11:48:37 +0000

    updating webpack config

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
             presets: ["@babel/preset-env", "@babel/preset-react"],
             plugins: [
               "@babel/plugin-proposal-class-properties",
-              "babel-plugin-styled-components"
+              ["babel-plugin-styled-components", { displayName: false }]
             ]
           }
         }
