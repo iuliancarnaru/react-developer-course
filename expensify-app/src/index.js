1f94b19ae8b9fcdec7968d0730595ce645972786
iuliancarnaru <iulian.carnaru@gmail.com>
2020-02-14 16:29:28 +0000

    updating index

--- expensify-app/src/index.js
+++ expensify-app/src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import "normalize.css";
 import AppRouter from "./routes/AppRouter";
+import configureStore from "./redux/store/configureStore";
+
+const store = configureStore();
+
+console.log(store.getState());
 
 render(<AppRouter />, document.getElementById("root"));
