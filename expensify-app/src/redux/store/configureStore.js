349a265afa16acfda3eaf0ddf95c6f34ab5cdd96
iuliancarnaru <iulian.carnaru@gmail.com>
2020-02-15 21:21:52 +0000

    adding redux logger and support for dev tools

--- expensify-app/src/redux/store/configureStore.js
+++ expensify-app/src/redux/store/configureStore.js
@@ -1,14 +1,22 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import expensesReducer from "../reducers/expenses";
 import filtersReducer from "../reducers/filters";
 
 export default () => {
   // Store creation
-  const store = createStore(
+  let middlewares = [];
+
+  if (process.env.NODE_ENV === `development`) {
+    const { logger } = require(`redux-logger`);
+    middlewares.push(logger);
+  }
+
+  const store = compose(applyMiddleware(...middlewares))(createStore)(
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   return store;
