108e431fbd2dab7cc64efbf9bc7a647b9468af55
iuliancarnaru <iulian.carnaru@gmail.com>
2020-02-15 21:05:51 +0000

    adding edit expense page

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
+import { navigate } from "@reach/router";
+import ExpenseForm from "./ExpenseForm";
+import { editExpense, removeExpense } from "../redux/actions/expenses";
 
-const EditExpensePage = ({ id }) => {
+const EditExpensePage = props => {
   return (
     <div>
-      <p>EditExpensePage with id {id}</p>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={expense => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          setTimeout(() => navigate("/"), 0);
+        }}
+      />
+      <button
+        onClick={() => {
+          props.dispatch(removeExpense({ id: props.expense.id }));
+          setTimeout(() => navigate("/"), 0);
+        }}
+      >
+        Remove
+      </button>
     </div>
   );
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find(expense => expense.id === props.id)
+});
+
+export default connect(mapStateToProps)(EditExpensePage);
